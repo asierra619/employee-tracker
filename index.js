dde3f8c9c9be4de5a2f4f66bb7a47a0672857b87
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-03-12 17:51:41 -0700

    added code for inquirer prompts and running the program in the terminal

--- index.js
+++ index.js
@@ -1,10 +1,123 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+require('dotenv').config();
+
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // TODO: Add MySQL password here
-    password: '',
-    database: 'movies_db'
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
   },
-  console.log(`Connected to the movies_db database.`)
+  console.log(`Connected to the employee_db database.`))
+
+  // db.query('SELECT * FROM favorite_books', function (err, results) {
+  //   console.log(results);
+  // });
+  
+
+function menu() {
+  inquirer.prompt(
+    [
+      {
+        type: 'list',
+        name: 'option',
+        message: 'Choose one of the following.',
+        choices: [
+          'View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'
+        ]
+      }
+    ]
+  ).then (answers => {
+    if (answers.option === 'View All Departments'){
+      viewDepartments()
+    }
+    if (answers.option === 'View All Roles'){
+      viewRoles()
+    }
+    if (answers.option === 'View All Employees'){
+      viewEmployees()
+    }
+    if (answers.option === 'Add a Department'){
+      addDepartment()
+    }
+    if (answers.option === 'Add a Role'){
+      addRole()
+    }
+    if (answers.option === 'Add an Employee'){
+      addEmployee()
+    }
+  })
+}
+
+function viewDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+    console.table(results);
+    return menu()
+  });
+}
+function viewRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
+    console.table(results);
+    return menu()
+  });
+}
+function viewEmployees() {
+    db.query('SELECT * FROM employee', function (err, results) {
+    console.table(results);
+    return menu()
+  });
+}
+
+function addDepartment() {
+  inquirer.prompt(
+    [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the Department?'
+      }
+    ]
+  ).then (answers => {
+    db.query('INSERT into department SET ?', answers, function (err, results) {
+      console.table(results);
+      return menu()
+    });
+  });
+}
+function addRole() {
+  db.query('SELECT * FROM department', (err, results) => {
+    // dpt is department
+    const departmentArray = results.map(dpt => ({
+      name: dpt.name, value: dpt.id
+    }))
+
+  inquirer.prompt(
+    [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the position Title?'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the Salary?'
+      },
+      {
+        type: 'list',
+        name: 'department_id',
+        message: 'Which Department does it belong to?',
+        choices: departmentArray
+      },
+    ]
+  ).then (answers => {
+    db.query('INSERT into role SET ?', answers, function (err, results) {
+      console.table(results);
+      return menu()
+    });
+  });
+})
+}
+
+menu();
