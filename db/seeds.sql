e03f2b93fd02b5fb67180f741c4a02fbd336aec6
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-03-12 17:50:30 -0700

    set up schema and starter seeds

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO favorite_books (book_name)
+-- VALUES ("The Great Gatsby"),
+--        ("Huckleberry Finn"),
+--        ("100 Years of Solitude"),
+--        ("Things Fall Apart"),
+--        ("Crime and Punishment"),
+--        ("Moby Dick"),
+--        ("Decameron");
+       
+
+INSERT INTO department (name)
+VALUES ('TIRES'),
+('FRONT END'),
+('MERCH');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Installer', 50000, 1),
+('Cashier', 50000, 2),
+('Driver', 52000, 3);
+
